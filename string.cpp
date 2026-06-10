b60e1e5b3d2c71bc27481cc05fa2365917d0d7db
AbdullahArif87-tech <abdulla8787ii@gmail.com>
2026-06-10 19:50:46 +0500

    added stuff

---
+++ string.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    string name ,name1;
+    int count=0;
+    cout<<"Enter string which you want to find largest word: ";
+    getline(cin,name);
+    name1=name;
+    
+    cout<<name1;
+    
+}
